d64981e95ac6450d07b78dbdebf69e5e2e225635
wytzz <wytzeaz@hotmail.com>
2018-11-09 14:22:09 +0100

    First Commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Wytze Dijkstra on 09/11/2018.
+//  Copyright © 2018 Wytze Dijkstra. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word : String
+    var incorrectMovesRemaining : Int
+    var guessedLetters : [Character]
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
+
+
+
+
+
